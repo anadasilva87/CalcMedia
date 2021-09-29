da494e43628ef78e52c6c413d0fab2e4767d444f
Ana Rita Rodrigues <anadasilva87@gmail.com>
2021-09-29 00:08:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,15 +14,14 @@ function calculoMedia() {
     document.getElementById("reprovado").style.display = "none";
   
     if (notaFinal >= media) {
-      document.getElementById("aprovado").style.display = "flex";
+      document.getElementById("aprovado").style.display = "block";
       document.getElementById("aprovado").innerHTML =
         '<span class="apto">PARABÉNS! </span>' +
         "Você foi aprovado com média: " +
-        notaFinal;
+        notaFinal + '<img width="400" height="200"  src="https://i.pinimg.com/originals/a8/8c/c8/a88cc85ba0b2cbf03eef5329ba89a9b6.gif">';
     } else {
       document.getElementById("reprovado").innerHTML =
-        '<span class="rep">REPROVADO! </span>' + "Sua média foi: " + notaFinal;
-      document.getElementById("reprovado").style.display = "FLEX";
+          '<span class="rep">REPROVADO! </span>' + "Sua média foi: " + notaFinal + '<iframe width="400" height="200" src="https://www.youtube.com/embed/0-sQrPu26zc?controls=0&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        document.getElementById("reprovado").style.display = "block";
+      }
     }
-  }
-  
